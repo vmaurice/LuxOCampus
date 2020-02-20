871e78bd160e2c4b89fe7f5bb1b44e8a9b066225
vmaurice <vincent.maurice@outlook.fr>
2020-02-20 10:32:48 +0100

    ajout connection google api, recuperation du token, premier resultat simple

---
+++ lib/Google/google.h
@@ -0,0 +1,198 @@
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WebServer.h>
+
+#include <ArduinoJson.h>
+
+
+String client_id = "569902583187-t5trsl9osrndemokbgcne8s83o9ibmul.apps.googleusercontent.com";
+String client_secret = "hfZ920yJ8owIawWlUvJfAGTY";
+String url_google;
+String user_code;
+String device_code;
+
+
+struct HttpResponse {
+  int httpResponseCode;
+  String httpResponse;
+};
+
+
+HTTPClient http;
+
+
+HttpResponse httpPost(String host, String url) {
+  HttpResponse request;
+  
+  http.begin("https://oauth2.googleapis.com" + host);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+              
+
+  request.httpResponseCode = http.POST(url);
+
+  if (request.httpResponseCode > 0) {
+    request.httpResponse = http.getString();                       //Get the response to the request
+    //Serial.println(request.httpResponseCode);   //Print return code
+    Serial.println(request.httpResponse);
+  } else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(request.httpResponseCode);
+  }
+
+  http.end();
+
+  return request;
+
+
+}
+
+
+HttpResponse httpGet(String url) {
+  HttpResponse request;
+
+  http.begin(url);
+
+  request.httpResponseCode = http.GET();
+
+  if (request.httpResponseCode > 0) {
+    request.httpResponse = http.getString();                       //Get the response to the request
+          
+    Serial.println(request.httpResponseCode);   //Print return code
+    Serial.println(request.httpResponse);
+  } else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(request.httpResponseCode);
+  }
+
+  http.end();
+
+  return request;
+}
+
+
+void setup_google(WiFiServer server, WiFiClient client) {
+    HTTPClient http;
+
+    // We now create a URI for the request
+    String url = "client_id=" + client_id + 
+    "&scope=https://www.googleapis.com/auth/calendar.readonly";
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    http.begin("https://oauth2.googleapis.com/device/code");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+
+    int httpResponseCode = http.POST(url);
+
+    String response;
+
+    if (httpResponseCode > 0) {
+      response = http.getString();                       //Get the response to the request
+ 
+      Serial.println(httpResponseCode);   //Print return code
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    const size_t capacity = JSON_OBJECT_SIZE(5) + 220;
+    DynamicJsonDocument doc(capacity);
+    
+    DeserializationError error = deserializeJson(doc, response);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
+    }
+
+    http.end();
+
+    Serial.println(doc["verification_url"].as<char*>());
+    Serial.println(doc["user_code"].as<char*>());
+    url_google = doc["verification_url"].as<String>();
+    device_code = doc["device_code"].as<String>();
+
+
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println();
+
+    // the content of the HTTP response follows the header:
+    client.print("<a href=\"" + url_google + "\"> Se connecter a Google. </a><br>");
+    client.print("<p> code a copier : " + device_code + "</p><br>");
+
+    // The HTTP response ends with another blank line:
+    client.println();
+
+    httpResponseCode = -1;
+    
+    url = "client_id=" + client_id + 
+    "&client_secret=" + client_secret +
+    "&device_code=" + device_code +
+    "&grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code";
+
+    Serial.println(url);
+
+    while (httpResponseCode != 200) {
+      http.begin("https://oauth2.googleapis.com/token");
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+
+      httpResponseCode = http.POST(url);
+
+      //Serial.println(httpResponseCode);
+
+      delay(5000);
+    }
+
+    if (httpResponseCode > 0) {
+      response = http.getString();                       //Get the response to the request
+ 
+      Serial.println(httpResponseCode);   //Print return code
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+
+
+
+    const size_t capacity2 = JSON_OBJECT_SIZE(5) + 400;
+    DynamicJsonDocument doc2(capacity2);
+    
+    error = deserializeJson(doc2, response);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
+    }
+
+    String access_token = doc2["access_token"].as<String>();
+
+    Serial.println(access_token);
+
+    url = "?access_token=" + access_token;
+
+    http.begin("https://www.googleapis.com/calendar/v3/users/me/calendarList/primary" + url);
+
+    httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+      response = http.getString();                       //Get the response to the request
+ 
+      Serial.println(httpResponseCode);   //Print return code
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+    
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,32 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
 WiFiManager wm; // global wm instance
-char * ssid = "ESP32";
+const char* ssid = "ESP32";
 
 // OPTION FLAGS
 bool TEST_CP  = true; // always start the configportal, even if ap found
 bool TEST_NET = true; // do a network test after connect, (gets ntp time)
+
+
+
+#include "google.h"
+
+
+WiFiServer server(80);
+String header;
+
+
+HttpResponse request;
+String url;
+String access_token = "";
+
+// In JsonFile
+const size_t capacity = JSON_OBJECT_SIZE(5) + 220;
+DynamicJsonDocument jsonConnect(capacity);
+
+const size_t capacity2 = JSON_OBJECT_SIZE(5) + 400;
+DynamicJsonDocument jsonToken(capacity2);
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -57,9 +78,9 @@ void setup() {
      Serial.println("connected...yeey :)");
   }
 
+  server.begin();
 
-
-  
+  Serial.println("Server started");
 
 
 
@@ -67,4 +88,124 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+  WiFiClient client = server.available();   // listen for incoming clients
+
+
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            if (header.indexOf("GET /google") >= 0) {
+              // We now create a URI for the request
+              url = "client_id=" + client_id + 
+              "&scope=https://www.googleapis.com/auth/calendar.readonly";
+
+              Serial.print("Requesting URL: ");
+              Serial.println(url);
+
+              request = httpPost("/device/code", url);
+
+              
+              
+              DeserializationError error = deserializeJson(jsonConnect, request.httpResponse);
+              if (error) {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.c_str());
+              }
+
+              Serial.println(jsonConnect["verification_url"].as<char*>());
+              Serial.println(jsonConnect["user_code"].as<char*>());
+              url_google = jsonConnect["verification_url"].as<String>();
+              user_code = jsonConnect["user_code"].as<String>();
+              device_code = jsonConnect["device_code"].as<String>();
+
+
+              // the content of the HTTP response follows the header:
+              client.print("<a target=\"_blank\" href=\"" + url_google + "\"> Se connecter a Google. </a><br>");
+              client.print("<p> code a copier : " + user_code + "</p><br></br>");
+
+              client.print("<a href=\"/result\"> Resultat </a></br>");
+
+
+            } else if (header.indexOf("GET /result") >= 0) {
+              if (access_token == "") {
+              
+                request.httpResponseCode = -1;
+      
+                url = "client_id=" + client_id + 
+                "&client_secret=" + client_secret +
+                "&device_code=" + device_code +
+                "&grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code";
+
+                Serial.println(url);
+
+                while (request.httpResponseCode != 200) {
+                  request = httpPost("/token", url);
+
+                  //Serial.println(request.httpResponseCode);
+
+                  delay(5000);
+                }
+
+                DeserializationError error = deserializeJson(jsonToken, request.httpResponse);
+                if (error) {
+                  Serial.print(F("deserializeJson() failed: "));
+                  Serial.println(error.c_str());
+                  return;
+                }
+
+                access_token = jsonToken["access_token"].as<String>();
+
+                Serial.println(access_token);
+              }
+
+              request = httpGet("https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=10&orderBy=startTime&singleEvents=True&timeMin=2020-02-19T10:00:00Z&access_token=" + access_token);
+
+              client.print("<p> Les evenements </p></br>");
+              client.print(request.httpResponse);
+            
+            } else {
+              client.print("<a href=\"/google\"> Se connecter à google </a></br>");
+            }
+
+            
+            
+            // The HTTP response ends with another blank line:
+            client.println();
+
+            // break out of the while loop:
+            break;
+
+        } else {    // if you got a newline, then clear currentLine:
+          currentLine = "";
+        }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // close the connection:
+    header = "";
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+
 }
