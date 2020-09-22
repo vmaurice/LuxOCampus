833c4ccc801cb9534465fd834cb4af120c1cc938
vmaurice <vincent.maurice@outlook.fr>
2020-09-22 21:15:02 +0200

    update

--- lib/Google/google_credentials.h
+++ lib/Google/google_credentials.h
@@ -1,2 +1,2 @@
-String client_id = "";
-String client_secret = "";
+String client_id = "569902583187-t5trsl9osrndemokbgcne8s83o9ibmul.apps.googleusercontent.com";
+String client_secret = "hfZ920yJ8owIawWlUvJfAGTY";
