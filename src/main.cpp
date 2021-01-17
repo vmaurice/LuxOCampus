a3c7a7af577d9ebc0d9c288cbfc6b359fae89ce1
vmaurice <vincent.maurice@outlook.fr>
2021-01-17 23:26:23 +0100

    OTA done

--- src/main.cpp
+++ src/main.cpp
@@ -420,7 +420,11 @@ void setup()
 	server.on("/disconnect_google", handleDisconnectGoogle);
 	server.on("/google", handleGoogle);
 	server.on("/update", handleUpdate);
-	server.on("/update_post", handleUpdatePost);
+	server.on("/update_post", HTTP_POST, []() {
+    	server.sendHeader("Connection", "close");
+		server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+		ESP.restart();
+		}, handleUpdatePost);
 
 	server.begin();
 }
@@ -1030,25 +1034,27 @@ void handleUpdate()
 }
 
 void handleUpdatePost() {
-	HTTPUpload& upload = server.upload();
-	if (upload.status == UPLOAD_FILE_START) {
-		Serial.printf("Update: %s\n", upload.filename.c_str());
-		if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-			Update.printError(Serial);
-		}
-	} else if (upload.status == UPLOAD_FILE_WRITE) {
-		// flashing firmware to ESP 
-		if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-			Update.printError(Serial);
-		}
-	} else if (upload.status == UPLOAD_FILE_END) {
-		if (Update.end(true)) { //true to set the size to the current progress
-			Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-		} else {
-			Update.printError(Serial);
-		}
-	}
 
-	server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
+	//Serial.println("Update Post page");
+
+	HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    } else {
+		Serial.println("Error update");
+	}
 }
